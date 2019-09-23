b8e7672e8c6a4de060889344377e15ba47ab5e87
alexjianghuiting <573502429@qq.com>
2019-09-23 11:15:13 -0700

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+mkdir deep_model
+mkdir dlib_model
+mkdir db
+cd dlib_model
+git clone https://github.com/AKSHAYUBHAT/TensorFace/blob/master/openface/models/dlib/shape_predictor_68_face_landmarks.dat
+cd deep_model
+wget http://www.robots.ox.ac.uk/~vgg/software/vgg_face/src/vgg_face_caffe.tar.gz
+tar -xvf vgg_face_caffe.tar.gz
+wget https://doc-0o-78-docs.googleusercontent.com/docs/securesc/2b95l2vfcbq7907m464e76kksdvatmea/63hl2qfc19kn2uru1hcnprb6f4lk3ivc/1569254400000/14628699919541684635/03075385467390535159/1W_moLzMlGiELyPxWiYQJ9KFaXroQ_NFQ?e=download&nonce=7og9ntc7nko7m&user=03075385467390535159&hash=2cq0f9924fnqknfs4b6u4pa1pe48fp9f
+wget https://doc-0g-78-docs.googleusercontent.com/docs/securesc/2b95l2vfcbq7907m464e76kksdvatmea/6cogd8rqhisgtkdcj85f8f6v8aftstj7/1569261600000/14628699919541684635/03075385467390535159/1AW3WduLk1haTVAxHOkVS_BEzel1WXQHP?e=download
